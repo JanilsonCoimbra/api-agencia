3526fd159128c324a65dcf1c6887d99b57fd14c7
Janilson Coimbra <77218845+JanilsonCoimbra@users.noreply.github.com>
2022-03-04 12:26:36 -0300

    Delete Desktop directory

--- Desktop/SpringBoot/SistemaVendas/src/main/java/com/recode/model/DestinoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recode.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DestinoRepository extends JpaRepository<Destino, Long> {
-
-}
