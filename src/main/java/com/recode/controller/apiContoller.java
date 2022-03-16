f80548468382569f0f389ffa9040a6a7722ab988
Janilson Coimbra <janilson.coimbra@gmail.com>
2022-03-15 21:52:27 -0300

    Implements methods post e put

--- src/main/java/com/recode/controller/apiContoller.java
+++ src/main/java/com/recode/controller/apiContoller.java
@@ -5,12 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.recode.dto.DestinoDTO;
+import com.recode.model.Destino;
 import com.recode.service.apiService;
 
 @RestController
@@ -29,5 +32,15 @@ public class apiContoller {
 	@GetMapping("/{id}")
 	public DestinoDTO getDestino(@PathVariable Long id) {
 		return sevice.findById(id);
+	}
+	@PutMapping("/")
+	public Destino updateDest(@RequestBody DestinoDTO dest){
+		return sevice.updateDest(dest);
+
+	}
+	@PostMapping("/")
+	public String InsertDest(@RequestBody DestinoDTO dest){
+		return sevice.InsertDest(dest);
+
 	}
 }
--- src/main/java/com/recode/service/apiService.java
+++ src/main/java/com/recode/service/apiService.java
@@ -3,6 +3,7 @@ package com.recode.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,4 +32,36 @@ public class apiService {
 		return DestDTO;
 	}
 
+	@Transactional
+	public Destino updateDest(DestinoDTO dest){
+
+		Destino destin = repository.findById(dest.getDestinoId()).get();
+		destin.setDestinoId(dest.getDestinoId());
+		destin.setDestinoName(dest.getDestinoName());
+		destin.setDestinoDescricao(dest.getDestinoDescricao());
+		destin.setDestinoValor(dest.getDestinoValor());
+		destin.setDestinoFoto(dest.getDestinoFoto());
+		destin.setDestinoData(dest.getDestinoData());
+		destin.setDestinoRetornoData(dest.getDestinoRetornoData());
+		repository.saveAndFlush(destin);
+		return destin;
+	}
+
+	@Transactional
+	public String InsertDest(DestinoDTO dest){
+
+		Destino destin = new Destino();
+		destin.setDestinoName(dest.getDestinoName());
+		destin.setDestinoDescricao(dest.getDestinoDescricao());
+		destin.setDestinoValor(dest.getDestinoValor());
+		destin.setDestinoFoto(dest.getDestinoFoto());
+		destin.setDestinoData(dest.getDestinoData());
+		destin.setDestinoRetornoData(dest.getDestinoRetornoData());
+		repository.saveAndFlush(destin);
+		return "Sucesso";
+	}
+	/*
+
+
+*/
 }
